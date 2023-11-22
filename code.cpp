33f1805ab14b42506912505a2685540c23288433
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2023-11-22 20:01:19 +0500

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,13 @@
+//"	Declare two float variables numerator and denominator.
+
+#include<iostream>
+using namespace std;
+int main(){
+	float a = 14.24;
+	float b = 7.12;
+	
+	cout << "Numerator = " << a << endl;
+	cout << "Denominator = " << b << endl;
+	
+	return 0;
+}
