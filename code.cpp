53b73e449a5070b8a6fae2d9d1c4f16d52ead138
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2023-11-25 20:21:51 +0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -5,9 +5,11 @@ using namespace std;
 int main(){
 	float a = 14.24;
 	float b = 7.12;
+	float result = a/b;
 	
 	cout << "Numerator = " << a << endl;
 	cout << "Denominator = " << b << endl;
+	cout << "Answer = " << result << endl;
 	
 	return 0;
 }
